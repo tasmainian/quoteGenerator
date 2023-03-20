8bde7dc84bb8a4781ccee52b7abd34c3359cae30
Tasmiha <thassan@microsoft.com>
2023-03-20 13:18:03 -0700

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import React, {useState, useEffect} from 'react';
 
 export default function App() {
+  const [quote, setQuote] = useState("")
+
+  const randomQuote = () => {
+    fetch("https://api.quotable.io/random")
+    .then(res => res.json())
+    .then(result => {
+      console.log(result)
+      setQuote(result.content)
+    })
+  }
+
+  useEffect(() => {
+    randomQuote();
+  }, [])
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.quoteContainer}>
+        <Text style={styles.quoteText}>Quote of the day </Text>
+        <Text style={styles.innerText}>{quote}</Text>
+        <TouchableOpacity onPress={randomQuote} style={styles.button}>
+          <Text style={styles.buttonText}>New Quote</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -13,8 +34,41 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#5372F0',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  quoteContainer: {
+    width: '90%',
+    backgroundColor: '#fff',
+    borderRadius: 20,
+    padding: 20
+  },
+  quoteText: {
+    textAlign: 'center',
+    fontSize: 26,
+    fontWeight: '600',
+    color: '#333',
+    marginBottom: 20
+  },
+  innerText: {
+    color:'#000',
+    fontSize: 16,
+    lineHeight: 26,
+    letterSpacing: 1.1,
+    fontWeight: '400',
+    textAlign: 'center',
+    marginBottom: 10
+  },
+  button: {
+    backgroundColor: '#5372F0',
+    padding: 20,
+    borderRadius: 30,
+    marginVertical: 20
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 18,
+    textAlign: 'center'
+  }
 });
